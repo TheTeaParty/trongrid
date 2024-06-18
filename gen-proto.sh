3a79f9c3f0a1d6d226b56c0747a967b4cabc59d0
Vlad Gorokhov <vlad.gorokh@gmail.com>
2024-06-18 22:22:00 +0100

    Add protobuf code (#2)
    * submodules added
    
    * proto gen script added
    
    * fix generation script
    
    * some regenerate

---
+++ gen-proto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INCLUDES="-I=./api/proto/tron -I/usr/lib -I./api/proto/googleapis"
+FLAGS="--go_out=./pkg/tronpb --go_opt paths=source_relative --go-grpc_out=./pkg/tronpb --go-grpc_opt=paths=source_relative"
+OLD_PACKAGE="github.com/tronprotocol/grpc-gateway"
+NEW_PACKAGE="github.com/TheTeaParty/trongrid/pkg/tronpb"
+PROTO_DIR="./api/proto/tron"
+
+
+# Run the Go script to replace the go_package option
+go run util/replacer/main.go $PROTO_DIR $OLD_PACKAGE $NEW_PACKAGE
+
+# Run protoc
+protoc ${INCLUDES} ${FLAGS} ${PROTO_DIR}/core/*.proto ${PROTO_DIR}/core/contract/*.proto ${PROTO_DIR}/api/*.proto
+
+# Fix the imports
+mv pkg/tronpb/core/contract/* pkg/tronpb/core/
+
+# Restore the go_package option
+go run util/replacer/main.go $PROTO_DIR $NEW_PACKAGE $OLD_PACKAGE
+
+echo "Done"
